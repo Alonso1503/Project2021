24c76c291ec275f33243dc6e219a3e2332bcbe5e
Alonso <alonsoariasal15@.com>
2021-08-22 22:51:58 -0600

    Min 50

---
+++ src/database.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const { database } = require("./keys");
+
+const pool = mysql.createPool(database);
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("DATABASE CONNECTION LOST");
+    }
+    if (err.code === "ERR_CON_COUNT_ERROR") {
+      console.error("DATABASE HAS TOO MANY CONNECTIONS");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("DATABASE CONNECTION REFUSED");
+    }
+  }
+  if (connection) connection.release();
+  console.log("DB CONNECTED");
+  Return;
+});
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 //Iniciar
 app = express();
-
 //Configuraciones
 app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views"));
@@ -24,9 +23,15 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //Vbls globales
+app.use((req, res, next) => {
+  next();
+});
 //Rutas
 app.use(require("./routes"));
+app.use(require("./routes/authentication"));
+app.use("links", require("./routes/links"));
 //Public
+app.use(express.static(path.join(__dirname, "public")));
 //Server
 app.listen(app.get("port"), () => {
   console.log("Server on port", app.get("port"));
